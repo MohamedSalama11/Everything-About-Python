fb04b01656b76aa4d37ae37bfa038be235e0a7a7
MohamedSalama11 <MohamedSalama11@github.com>
2022-11-13 14:17:06 +0200

    Mohamed

--- Mohamed.py
+++ Mohamed.py
@@ -2840,7 +2840,72 @@
 
 
 ####### Caesar Cipher #######
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+while True:
+    direction = input(
+        "Type 'encode' to encrypt, type 'decode' to decrypt:\n".lower())
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    def encrypt(plain_text, shift_amount):
+        cipher_text = ""
+        for letter in plain_text:
+            if letter in alphabet:
+                position = alphabet.index(letter)
+                new_position = position + shift_amount
+                new_letter = alphabet[new_position]
+                cipher_text += new_letter
+            else:
+                cipher_text += letter
+        print(f"The Encoded Text Is {cipher_text}.")
+
+
+    def decrypt(plain_text, shift_amount):
+        decrypt_text = ""
+        for letter in plain_text:
+            if letter in alphabet:
+                position = alphabet.index(letter)
+                new_position = position - shift_amount
+                new_letter = alphabet[new_position]
+                decrypt_text += new_letter
+            else:
+                decrypt_text += letter
+        print(f"The Decoded Text Is {decrypt_text}.")
+
+
+    if direction == "encode":
+        encrypt(plain_text=text, shift_amount=shift)
+    elif direction == "decode":
+        decrypt(plain_text=text, shift_amount=shift)
+    check = input("Type 'Yes' If You Want To Go Again. Otherwise Type 'No'. \n").capitalize()
+    if check == "Yes":
+        continue
+    else:
+        print("Thank You!")
+        break
 
 #################################################### Git Commands ####################################################
 # git clone                                             => To Clone (Copy) The Project From Remote Repo (GitHub) To Local Repo (PC)
