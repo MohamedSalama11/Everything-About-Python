e31e64b1b9cc8a9d2d9374af754847a6723f7eb3
MohamedSalama11 <MohamedSalama11@github.com>
2022-12-26 17:33:36 +0200

    fsda

--- Mohamed.py
+++ Mohamed.py
@@ -3166,22 +3166,22 @@
 # git tag -d tagName                                    => To Remove A Tag Localy
 # git push origin --delete tagName                      => To Remvoe A Tag Remotely
 
-# Drawing Bill Gates
 
+# Drawing Bill Gates
 # from sklearn.preprocessing import scale
-# import cv2
 # pic = canvas.sketch_from_svg(
 #     r"D:\Programming\Python\Python Projects\7-removebg-preview.svg")
 # pic.draw()
 
 
-#### Version 2 ####
+#### Drawing Elzero ####
 # from sketchpy import canvas
 # pic = canvas.sketch_from_svg(
 #     r"D:\Programming\Python\Python Projects\elzero.svg", scale=600)
 # pic.draw()
 
-########### Convert The Image Into Sketch ##########
+# ########### Convert The Image Into Sketch ##########
+# import cv2
 # image = cv2.imread(
 #     "cristi.jpg")
 # grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -3190,3 +3190,55 @@
 # invertedblur = cv2.bitwise_not(blur)
 # sketch = cv2.divide(grey_img, invertedblur, scale=256.0)
 # cv2.imwrite("test2.png", sketch)
+
+
+########### Car Game ############
+# from ursina import *
+# import random
+
+# app = Ursina()
+# camera.orthographic = True
+
+# camera.fov = 10
+
+# car = Entity(model='quad', texture='car',
+#              collider='box', scale=(2, 1), rotation_z=-90, y=-3)
+
+# road1 = Entity(model='quad', texture='road', scale=15, z=1)
+# road2 = duplicate(road1, y=15)
+# pair = [road1, road2]
+
+# enemies = []
+
+
+# def newEnemy():
+#     val = random.uniform(-2, 2)
+#     new = duplicate(car, texture='enemy', x=2*val, y=25, color=color.random_color(),
+#                     rotation_z=90 if val < 0 else -90)
+#     enemies.append(new)
+#     invoke(newEnemy, delay=0.5)
+
+
+# newEnemy()
+
+
+# def update():
+#     car.x -= held_keys['a']*5*time.dt
+#     car.x += held_keys['d']*5*time.dt
+#     for road in pair:
+#         road.y -= 6*time.dt
+#         if road.y < -15:
+#             road.y += 30
+#     for enemy in enemies:
+#         if enemy.x < 0:
+#             enemy.y -= 10 * time.dt
+#         else:
+#             enemy.y -= 5 * time.dt
+#         if enemy.y < -10:
+#             enemies.remove(enemy)
+#             destroy(enemy)
+#     if car.intersects().hit:
+#         car.shake()
+
+
+# app.run()
