26577b7d6c47ba3e12bc606635dca0ab5502bf81
Mohamed Salama <MohamedSalama11>
2023-06-23 06:49:47 +0300

    UPDATE

--- Every_Thing_About_MySQL.py
+++ Every_Thing_About_MySQL.py
@@ -17,7 +17,7 @@
 
 # Query => is the line of code you write with SQL language
 
-# When you learn SQL language with any DBMS like mysql it's not that hard to switch to other DBMS that also uses sql language like ( SQLite or PostgreSQL, Oracle )
+# When you learn SQL language with any DBMS like mysql it's not that hard to switch to other DBMS that also uses SQL language like ( SQLite or PostgreSQL, Oracle )
 
 # There's alot of data types in mysql like varchar (string), int (numbers)
     # varchar(10)   => this means you can write 10 characters maximum ("Mohamed")
@@ -25,23 +25,28 @@
 
 # CRUD => it stands for Create Read Update Delete 
 
+## String Functions ##
+# 1- CONCAT(column_one, column_two, string, ...)                  => Combine data for cleaner output (you should use SELECT before it to work or it will raise an error)
+# 2- CONCAT_WS(column_one, column_two, string, ...)               => Stands for concat with separator, it means that it will put a separator you chooose between every values you put
+# 3- SUBSTRING(column_name, start position, length)               => It takes a single larger string and returns a smaller part of that string  == SUBSTR()
+
 # Notes
 # 1- is sql case sensitive? It depends on the database management system being used. In general, SQL is not case sensitive for keywords such as SELECT, FROM, WHERE, etc
 # However, it may be case sensitive for identifiers such as table and column names, depending on how the database has been configured or the collation sequence being used
     # For example, in some databases, "mytable" and "MyTable" could refer to different tables.
 # 2- my sql doesn't care about new lines
-# 3- A database is just really a bunch of tables at least in realtional databases, tables holding the data
+# 3- A database is just really a bunch of tables at least in relational databases, tables holding the data
 # 4- Table is a collection of related data held in a structured format within a database, it contains columns and rows
-# 5- When you put ; in the end of a line you till SQL this is the end of the line or the query
+# 5- When you put ; in the end of a line you till SQL this is the end of the command or the query
 # 6- Null in SQL doesn't means zero it means no value at all or unknown
-# 7- Before you perform update and delete operations be sure selecting the right data you want 
+# 7- Before you perform update and delete operations be sure selecting the right data you want to update or delete
 
 ############# Commands #############
 # 01- show databases;                                                                                     => To show the databases you created
 # 02- CREATE DATABASE <name>;                                                                             => To create a database
 # 03- DROP DATABASE   <name>;                                                                             => Will remove the database and it's content
 # 04- USE <database name>;                                                                                => To work with the database we choose
-# 05- SELECT database()                                                                                   => To show you the database you are working in
+# 05- SELECT database();                                                                                  => To show you the database you are working in
 # 06- CREATE TABLE table_name (                                                                           => To create a table (create table if not exists)
 #   column_name data_type,
 #   column_name data_type      
@@ -54,7 +59,7 @@
 #   column_name data_type DEFAULT 'Mohamed',
 #   column_name data_type DEFAULT 10
 # );
-# 09- CREATE TABLE table_name (                                                                           => To create a table, primary key means that this column shouldn't have the same value,
+# 09- CREATE TABLE table_name (                                                                           => To create a table, PRIMARY KEY means that this column shouldn't have the same value,
 #   column_name data_type PRIMARY KEY,                                                                       the value of this column should be unique
 #   column_name data_type
 # );
@@ -76,12 +81,12 @@
 # 17- SELECT * FROM table_name                                                                            => This tells SQL give me everything you have in this specified table
 # 18- INSERT INTO table_name(columne_one, columne_two) VALUES (value1, value1), (value2, value2)          => This is for multiple inserting
 # 19- SELECT * FROM table_name                                                                            => This means give me all the data in all columns
-# 20- SELECT column_name FROM table_name                                                                  => This means give me only the data in this columns name
+# 20- SELECT column_name FROM table_name                                                                  => This means give me only the data in this column name
 # 21- SELECT column_name, column_name2 FROM table_name                                                    => This means give me the data in these two columns
 # 22- SELECT * FROM table_name WHERE column_name="value"                                                  => IT will search for "value" and return all the rows that have it
 # 23- SELECT column_name AS col FROM table_name                                                           => AS here for aliases and can rename the column name temporary in the output
 # 24- UPDATE table_name SET column_name="value"                                                           => This means update everything in column_name to value ( Always use WHERE Clause to specify the value you want to change )
 # 25- UPDATE table_name SET column_name="value", column_name2="value"                                     => To update multiple values
 # 26- DELETE FROM table_name                                                                              => This will delete every row from the table, will empty the table but it's still exists ( don't use it like this )
-# 27- DELETe FROM table_name WHERE column_name="value"                                                    => This will delete only the specified value
+# 27- DELETE FROM table_name WHERE column_name="value"                                                    => This will delete only the specified value
 
