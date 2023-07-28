d13411c5523da7e04f77aa1ba39c67e26f024636
MohamedSalama11 <salamaM2021@gmail.com>
2023-07-28 22:51:35 +0300

    Networking

--- Netwrok+_Certification_Notes.py
+++ Netwrok+_Certification_Notes.py
@@ -1,1 +1,28 @@
+# The whole purpose of networking is to send data from one machine to another.
+# Switches => A device that connects network devices together, Is just a next generation of hubs that combine hubs with another device called a bridge, and they can use logic
+    # to learn which physical ports are attached to which devices based on their mac addresses and in this way they can send data to specific devices in the network.
+# WAP ( wireless access point ) => Are devices that allows wireless devices to cnnect into a wired network, acts like a wireless hub.
+# WAN (wide area network) Links => It simply connects an internal netwrok to an external one.
+# IEEE 802.3 refers to Ethernet, IEEE 802.11 refer to Wi-Fi.
+# OSI model is also called OSI Stack and reference model.
+# IPX stands for Internetwork Packet Exchange but it's killed when ipv4 or ipv6 appeared.
+
+
+############# MAC Adresses #############
+# Physical addressing system of a device which operates on a logical topology.
+# It will be like 6 pairs of characters separated by :
+# Uses a 48-bit address assigned to a network interface card (NIC).
+# It consists of 12 characters in hexadecimal.
+# Each number or character is considered as 4 bits.
+# The first 6 chars is called Vendor Code and it's represetns who made this NIC.
+# The next 6 chars is called unique value and it's repersents the exact machine that belongs to.
+# Example => D2:51:F1:3A:34:65
+
+
+############# IPv4 #############
+
+
+############# IPv6 #############
+
+
 ############# Commands #############
